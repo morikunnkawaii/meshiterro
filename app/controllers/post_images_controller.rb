d9ab6536b77259eecfd4a6fc2f83d02c84f31dd8
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-29 03:11:33 +0000

    [add]投稿機能作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,9 +3,23 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_iamge.user_id = Current.user.id
+    @post_iamge.save
+    redirect_to post_images_path
+  end
+
   def index
   end
 
   def show
   end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
 end
