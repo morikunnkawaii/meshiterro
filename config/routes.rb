4c5a8943708e1ea0bf80292809a8184a55037711
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-31 07:43:03 +0000

    [Finish]いいね投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:new, :create, :show, :edit, :update] , path_names: {new: 'sign_up'}
   resource :session
